59a75e3a1110b868a5caffe48840bb2a747204e2
PososAgapo <bchen@kth.se>
2024-06-06 23:19:33 +0900

    add better trasnalted instruction

--- eval.py
+++ eval.py
@@ -85,7 +85,7 @@ def significance_test(guided_scores, general_scores):
         print("The difference is not statistically significant.")
         return False
 
-def is_contaminated(dataset, dataset_name):
+def is_contaminated(dataset, dataset_name, split_name):
     """Confirm wether GPT-3.5 is contaminated on a given dataset
     
     Args: 
@@ -117,13 +117,14 @@ def is_contaminated(dataset, dataset_name):
     bleurt_indicator = significance_test(bleurt_scores[0], bleurt_scores[1])
     rougel_indicator = significance_test(rougeL_scores[0], rougeL_scores[1])
     print('......Eval Results......\n', bleurt_scores, '\n', rougeL_scores, '\n')
-    
+    os.makedirs(f'contamination_result/{dataset_name}/{split_name}/', exist_ok=True)
+
     save_jsonl({
         "average bleurt score":  [sum(x)/len(references) for x in bleurt_scores],
         "rougeL score": [sum(x)/len(references) for x in rougeL_scores],
         "bleurt_indicator": bleurt_indicator,
         "rougel_indicator": rougel_indicator
-    }, f'out/{dataset_name}/train/data_contamination_result.jsonl')
+    }, f'contamination_result/{dataset_name}/{split_name}/data_contamination_result.jsonl')
     
     print("......Successfully saved eval result......")
     
