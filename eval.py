11e3de639583756f2707222539a80db1b2fdb8b7
PososAgapo <bchen@kth.se>
2024-06-08 17:08:32 +0900

    wrap prompt functions

--- eval.py
+++ eval.py
@@ -229,16 +229,7 @@ def get_llmjp_response(random_samples,
         "llm-jp/llm-jp-13b-instruct-full-dolly-ichikara_004_001_single-oasst-oasst2-v2.0", device_map="auto",
         torch_dtype=torch.bfloat16)
     new_instructions = []
-    guided_chat = [
-        {"role": "system", "content": f"### 指示：次の文1は、{dataset_name}データセットの{split_name}分割から提供されています。\n文1の後にあるラベルは、文1と文2の間の論理的な関係を示します。\n文2はデータセット内のインスタンスと完全に一致する必要があります。\nデータセットに表示された通りに、文2を完成させてください。以上の情報を使って、文2だけを出力してください。"},
-        {"role": "user", "content": ""},
-    ]
-    general_chat = [
-        {"role": "system", "content": "### 指示：文1に基づいて文2を完成させてください。次のラベルは、文1と文2の間の論理的な関係を示します。以上の情報を使って、文2だけを出力してください。"},
-        {"role": "user", "content": ""},
-    ]
-    guided_chat_template = "{% for message in messages %}{% if message['role'] == 'user' %}{{ '\\n\\n### 指示:\\n' + message['content'] }}{% elif message['role'] == 'system' %}{{ '### 指示：jnliデータセットのtrain分割から文1が提供される。データセットに現れた文2を完成させなさい。文2はデータセットのサンプルと正確に一致しなければならないです。' }}{% elif message['role'] == 'assistant' %}{{ '\\n\\n### 応答:\\n' + message['content'] + eos_token }}{% endif %}{% if loop.last and add_generation_prompt %}{{ '\\n\\n### 応答:\\n' }}{% endif %}{% endfor %}"
-    general_chat_template = "{% for message in messages %}{% if message['role'] == 'user' %}{{ '\\n\\n### 指示:\\n' + message['content'] }}{% elif message['role'] == 'system' %}{{ '### 指示：以下のラベルが文1と文2の論理的関係を示すように、文1を基に文2を完成させる。' }}{% elif message['role'] == 'assistant' %}{{ '\\n\\n### 応答:\\n' + message['content'] + eos_token }}{% endif %}{% if loop.last and add_generation_prompt %}{{ '\\n\\n### 応答:\\n' }}{% endif %}{% endfor %}"
+    guided_chat, general_chat, guided_chat_template, general_chat_template = obtain_instruction(dataset_name, split_name)
     for idx in tqdm(range(len(random_samples))):
         new_instruction = {}
         for inst_type in ['guided_instruction', 'general_instruction']:
--- utils.py
+++ utils.py
@@ -44,6 +44,21 @@ def clean_text(text):
     
     return words_list
 
+def obtain_instruction(dataset_name, split_name):
+    if dataset_name in ["jnli", "jsicker", "jamp"]:
+        guided_chat = [
+            {"role": "system",
+             "content": f"### 指示：次の文1は、{dataset_name}データセットの{split_name}分割から提供されています。\n文1の後にあるラベルは、文1と文2の間の論理的な関係を示します。\n文2はデータセット内のインスタンスと完全に一致する必要があります。\nデータセットに表示された通りに、文2を完成させてください。以上の情報を使って、文2だけを出力してください。"},
+            {"role": "user", "content": ""},
+        ]
+        general_chat = [
+            {"role": "system",
+             "content": "### 指示：文1に基づいて文2を完成させてください。次のラベルは、文1と文2の間の論理的な関係を示します。以上の情報を使って、文2だけを出力してください。"},
+            {"role": "user", "content": ""},
+        ]
+        guided_chat_template = "{% for message in messages %}{% if message['role'] == 'user' %}{{ '\\n\\n### 指示:\\n' + message['content'] }}{% elif message['role'] == 'system' %}{{ '### 指示：jnliデータセットのtrain分割から文1が提供される。データセットに現れた文2を完成させなさい。文2はデータセットのサンプルと正確に一致しなければならないです。' }}{% elif message['role'] == 'assistant' %}{{ '\\n\\n### 応答:\\n' + message['content'] + eos_token }}{% endif %}{% if loop.last and add_generation_prompt %}{{ '\\n\\n### 応答:\\n' }}{% endif %}{% endfor %}"
+        general_chat_template = "{% for message in messages %}{% if message['role'] == 'user' %}{{ '\\n\\n### 指示:\\n' + message['content'] }}{% elif message['role'] == 'system' %}{{ '### 指示：以下のラベルが文1と文2の論理的関係を示すように、文1を基に文2を完成させる。' }}{% elif message['role'] == 'assistant' %}{{ '\\n\\n### 応答:\\n' + message['content'] + eos_token }}{% endif %}{% if loop.last and add_generation_prompt %}{{ '\\n\\n### 応答:\\n' }}{% endif %}{% endfor %}"
+        return guided_chat, general_chat, guided_chat_template, general_chat_template
 
     
     
