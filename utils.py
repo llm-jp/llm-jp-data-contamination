ba18556aa003409202231c8fe7550d3053b39138
PososAgapo <bchen@kth.se>
2024-06-08 19:22:26 +0900

    add janli

--- utils.py
+++ utils.py
@@ -46,7 +46,7 @@ def clean_text(text):
 
 def obtain_instruction(dataset_name, split_name):
     chat_template = "{% for message in messages %}{% if message['role'] == 'user' %}{{ '\\n\\n### 指示:\\n' + message['content'] }}{% elif message['role'] == 'system' %}{{ '\\n\\n### 指示:\\n' + message['content'] }}{% elif message['role'] == 'assistant' %}{{ '\\n\\n### 応答:\\n' + message['content'] + eos_token }}{% endif %}{% if loop.last and add_generation_prompt %}{{ '\\n\\n### 応答:\\n' }}{% endif %}{% endfor %}"
-    if dataset_name in ["jnli", "jsicker", "jamp"]:
+    if dataset_name in ["jnli", "jsick", "jamp","janli"]:
         guided_chat = [
             {"role": "system",
              "content": f"次の文1は、{dataset_name}データセットの{split_name}分割から提供されています。\n文1の後にあるラベルは、文1と文2の間の論理的な関係を示します。\n文2はデータセット内のインスタンスと完全に一致する必要があります。\nデータセットに表示された通りに、文2を完成させてください。必ず文2のみを出力し、それ以外には何も含めないことを厳守してください。"},
@@ -98,6 +98,18 @@ def obtain_instruction(dataset_name, split_name):
             {"role": "user", "content": ""},
         ]
         return guided_chat, general_chat, chat_template
+    elif dataset_name in ["jcommonsenseqa"]:
+        guided_chat = [
+            {"role": "system",
+             "content": f"次の文は、{dataset_name}データセットの{split_name}分割から提供されています。\n質問を入力とし、回答を出力してください。回答の他には何も含めないことを厳守してください。回答が'はい'と'いいえ'で答えることができる場合、'YES'と'NO'で答えてください。\nデータセットに表示された通りに、回答を出力してください。必ず回答のみを出力し、それ以外には何も含めないことを厳守してください。"},
+            {"role": "user", "content": ""},
+        ]
+        general_chat = [
+            {"role": "system",
+             "content": f"質問を入力とし、回答を出力してください。回答の他には何も含めないことを厳守してください。回答が'はい'と'いいえ'で答えることができる場合、'YES'と'NO'で答えてください。\nデータセットに表示された通りに、回答を出力してください。必ず回答のみを出力し、それ以外には何も含めないことを厳守してください。"},
+            {"role": "user", "content": ""},
+        ]
+        return guided_chat, general_chat, chat_template
 
 
 def formalize_input(dataset_name,guided_chat, general_chat, inst_type, example):
@@ -127,6 +139,19 @@ def formalize_input(dataset_name,guided_chat, general_chat, inst_type, example):
             chat = general_chat
             chat[1]["content"] = f"{sent1}\n"
         return chat, sent1, sent2, instruction
+    elif dataset_name in ["jemhopqa", "jmmlu"]:
+        instruction = guided_chat[0]["content"] if inst_type == 'guided_instruction' else general_chat[0]["content"]
+        question = f"質問: {example['input']}"
+        answer = f"回答: {example['output']}"
+        if inst_type == 'guided_instruction':
+            chat = guided_chat
+            chat[1]["content"] = f"{question}\n"
+        else:
+            chat = general_chat
+            chat[1]["content"] = f"{question}\n"
+        return chat, question, answer, instruction
+
+
 
     
     
