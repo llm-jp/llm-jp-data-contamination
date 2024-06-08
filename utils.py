1e103fd3190944b22aab714c411413b7b5d3d05f
PososAgapo <bchen@kth.se>
2024-06-08 18:49:57 +0900

    add tempalte for QA

--- utils.py
+++ utils.py
@@ -86,18 +86,18 @@ def obtain_instruction(dataset_name, split_name):
             {"role": "user", "content": ""},
         ]
         return guided_chat, general_chat, chat_template
-    # elif dataset_name in ["jemhopqa"]:
-    #     guided_chat = [
-    #                 {"role": "system",
-    #                  "content": f"次の文は、{dataset_name}データセットの{split_name}分割から提供されています。\n質問を入力とし、回答を出力してください。回答の他には何も含めないことを厳守してください。回答が'はい'と'いいえ'で答えることができる場合、'YES'と'NO'で答えてください。\nデータセットに表示された通りに、回答を出力してください。必ず回答のみを出力し、それ以外には何も含めないことを厳守してください。"},
-    #                 {"role": "user", "content": ""},
-    #             ]
-    #             general_chat = [
-    #                 {"role": "system",
-    #                  "content": f"質問を入力とし、回答を出力してください。回答の他には何も含めないことを厳守してください。回答が'はい'と'いいえ'で答えることができる場合、'YES'と'NO'で答えてください。\nデータセットに表示された通りに、回答を出力してください。必ず回答のみを出力し、それ以外には何も含めないことを厳守してください。"},
-    #                 {"role": "user", "content": ""},
-    #             ]
-    #     return guided_chat, general_chat, chat_template
+    elif dataset_name in ["jemhopqa"]:
+        guided_chat = [
+                    {"role": "system",
+                     "content": f"次の文は、{dataset_name}データセットの{split_name}分割から提供されています。\n質問を入力とし、回答を出力してください。回答の他には何も含めないことを厳守してください。回答が'はい'と'いいえ'で答えることができる場合、'YES'と'NO'で答えてください。\nデータセットに表示された通りに、回答を出力してください。必ず回答のみを出力し、それ以外には何も含めないことを厳守してください。"},
+                    {"role": "user", "content": ""},
+                ]
+                general_chat = [
+                    {"role": "system",
+                     "content": f"質問を入力とし、回答を出力してください。回答の他には何も含めないことを厳守してください。回答が'はい'と'いいえ'で答えることができる場合、'YES'と'NO'で答えてください。\nデータセットに表示された通りに、回答を出力してください。必ず回答のみを出力し、それ以外には何も含めないことを厳守してください。"},
+                    {"role": "user", "content": ""},
+                ]
+        return guided_chat, general_chat, chat_template
 
 
 def formalize_input(dataset_name,guided_chat, general_chat, inst_type, example):
