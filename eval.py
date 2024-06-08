c410e084102da457407e698b05f3409a0a40a2cd
PososAgapo <bchen@kth.se>
2024-06-08 18:09:09 +0900

    fix input parameter bugs in formalize_input

--- eval.py
+++ eval.py
@@ -255,7 +255,7 @@ def get_llmjp_response(random_samples,
                 inst_type: {
                     "instruction": instruction,
                     "sentence1": sent1,
-                    "candidate": response.replace("文2：",""),
+                    "candidate": response,
                     "reference": sent2,
                     "label": example['output']
                 }
