3d2bce34028353c209696776cb400204c12e49fc
PososAgapo <bchen@kth.se>
2024-06-23 16:07:36 +0900

    add score output

--- test.py
+++ test.py
@@ -4,9 +4,13 @@ tokenizer = AutoTokenizer.from_pretrained("llm-jp/llm-jp-13b-instruct-full-dolly
 model = AutoModelForCausalLM.from_pretrained("llm-jp/llm-jp-13b-instruct-full-dolly-ichikara_004_001_single-oasst-oasst2-v2.0", device_map="auto", torch_dtype=torch.bfloat16)
 model.generation_config.output_scores = True
 model.generation_config.return_dict_in_generate = True
+# chat = [
+#     {"role": "system", "content": ""},
+#     {"role": "user", "content": "１匹の犬が車の後部に乗っています。"},
+# ]
 chat = [
-    {"role": "system", "content": "### 指示：jnliデータセットのtrain分割から文1が提供される。文2はデータセットのサンプルと正確に一致しなければならないです。"},
-    {"role": "user", "content": "１匹の犬が車の後部に乗っています。"},
+    {"role": "system", "content": "以下は、タスクを説明する指示です。要求を適切に満たす応答を書きなさい。"},
+    {"role": "user", "content": "自然言語処理とは何か"},
 ]
 text="""
 ### 指示：jnliデータセットのtrain分割から文1が提供される。データセットに現れた文2を完成させなさい。文2はデータセットのサンプルと正確に一致しなければならないです。
@@ -20,11 +24,32 @@ tokenized_input = tokenizer.apply_chat_template(chat, chat_template, add_generat
 with torch.no_grad():
     output = model.generate(
         tokenized_input,
-        max_new_tokens=100,
-        do_sample=True,
-        top_p=0.95,
-        temperature=0.7,
+        max_new_tokens=10,
+        top_k=0,
+        top_p=0,
+        temperature=0.0,
         repetition_penalty=1.05,
-    )[0]
+    )
+
 print(tokenizer.decode(output))
+import torch.nn.functional as F
+sequences = output.sequences[0][len(tokenized_input[0]):].unsqueeze(0)
+scores = torch.cat(output.scores, dim=0)
+
+shift_logits = scores[:-1, :].contiguous()
+shift_labels = sequences[:, 1:].contiguous().view(-1)
+
+loss = F.cross_entropy(shift_logits.view(-1, shift_logits.size(-1)), shift_labels, reduction='mean')
+
+perplexities = []
+for i in range(shift_labels.size(0)):
+    current_logits = scores[i, :].unsqueeze(0)
+    current_label = shift_labels[i].unsqueeze(0)
+    loss = F.cross_entropy(current_logits, current_label, reduction='mean')
+    perplexity = loss
+    perplexities.append(perplexity.item())
+    #perplexity = torch.exp(loss)
+# 输出每个step的困惑度
+for i, perp in enumerate(perplexities):
+    print(f"Step {i}: 困惑度 = {perp}")
 
