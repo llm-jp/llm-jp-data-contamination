269143d08335b436b3f588f54cca0aded6429bef
PososAgapo <bchen@kth.se>
2024-06-30 15:30:36 +0900

    extract pythia by dataset

--- distribution_validating.py
+++ distribution_validating.py
@@ -1,5 +1,7 @@
 from datasets import load_dataset
 import torch
 
-dataset_name = ["test_ArXiv.pt"]
-dataset = torch.load(dataset_name[0])
+dataset_name = ["ArXiv", "DM Mathematics", "Enron Emails", "EuroParl", "FreeLaw", "Github", "Gutenberg (PG-19)",
+                "HackerNews", "NIH ExPorter", "PhilPapers", "Pile-CC", "PubMed Abstracts", "PubMed Central", "StackExchange",
+                "Ubuntu IRC", "USPTO Backgrounds", "Wikipedia (en)"]
+split_name = ["train", "validation", "test"]
