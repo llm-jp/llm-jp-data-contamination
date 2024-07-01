fe4a5a9306786462d1b2e6e41079ce8e6aab97be
PososAgapo <bchen@kth.se>
2024-07-01 12:00:06 +0900

    add distribution analysis more feautres

--- distribution_validating.py
+++ distribution_validating.py
@@ -15,9 +15,28 @@ def batched_data(dataset, batch_size):
             break
         yield batch
 
+def figure_draw(data_dict, title):
+    plt.figure(figsize=(10, 5))
+    fig, axs = plt.subplots(len(data_dict), figsize=(10, 5 * len(data_dict)))
+    axs = np.atleast_2d(axs)
+    for ax, (dataset_name, dataset_loss) in zip(axs.flatten(), data_dict.items()):
+        for phase_name, phase_loss in dataset_loss.items():
+            weights = np.ones_like(phase_loss) / len(phase_loss)
+            ax.hist(phase_loss, bins=50, label=phase_name, alpha=0.5, weights=weights)
+        ax.set_title(f'{title} values histogram for {dataset_name}')
+        ax.set_xlabel(title)
+        ax.set_ylabel('Percentage')
+        ax.legend()
+    plt.tight_layout()
+    plt.savefig(f"{title}_histograms.png")
+    plt.show()
+
+
 
 def loss_collection(model, dataset):
     loss_collect = []
+    prob_collect = []
+    ppl_collect = []
     for batch in tqdm(batched_data(dataset, batch_size=8)):
         tokenized_inputs = tokenizer([item for item in batch],
                                      return_tensors="pt",
@@ -26,9 +45,21 @@ def loss_collection(model, dataset):
                                      max_length=2048)
         tokenized_inputs = {key: val.to("cuda") for key, val in tokenized_inputs.items()}
         outputs = model(**tokenized_inputs, labels=tokenized_inputs["input_ids"].cuda())
-        loss = outputs.loss
+        loss, logits = outputs.loss[:2]
+        probabilities = torch.nn.functional.log_softmax(logits, dim=-1)
         loss_collect.append(loss.item())
-    return loss_collect
+        all_prob = []
+        input_ids_processed = tokenized_inputs[0][1:]
+        for i, token_id in enumerate(input_ids_processed):
+            probability = probabilities[0, i, token_id].item()
+            all_prob.append(probability)
+        ppl = torch.exp(loss).item()
+        k_length = int(len(all_prob) * 0.2)
+        topk_prob = np.sort(all_prob)[:k_length]
+        pred = -np.mean(topk_prob).item()
+        prob_collect.append(pred)
+        ppl_collect.append(ppl)
+    return loss_collect, prob_collect, ppl_collect
 
 #dataset_name = ["ArXiv", "DM Mathematics", "Enron Emails", "EuroParl", "FreeLaw", "Github", "Gutenberg (PG-19)",
 #                "HackerNews", "NIH ExPorter", "PhilPapers", "Pile-CC", "PubMed Abstracts", "PubMed Central", "StackExchange",
@@ -49,32 +80,32 @@ tokenizer = AutoTokenizer.from_pretrained(
   cache_dir="./pythia-160m-deduped/step143000",
 )
 tokenizer.pad_token = tokenizer.eos_token
-loss_dicit = {}
+loss_dict = {}
+prob_dict = {}
+ppl_dict = {}
 for name in dataset_name:
-    loss_dicit[name] = {"train": [], "valid": [], "test": []}
+    loss_dict[name] = {"train": [], "valid": [], "test": []}
+    prob_dict[name] = {"train": [], "valid": [], "test": []}
+    ppl_dict[name] = {"train": [], "valid": [], "test": []}
     for split in split_name:
         if split in ["test", "valid"]:
             dataset = torch.load(f"by_dataset/{split}_{name}.pt")
-            loss_list = loss_collection(model, dataset)
-            loss_dicit[name][split].extend(loss_list)
+            loss_list, prob_list, ppl_list = loss_collection(model, dataset)
+            loss_dict[name][split].extend(loss_list)
+            prob_dict[name][split].extend(prob_list)
+            ppl_dict[name][split].extend(ppl_list)
         else:
             for i in range(1):
                 dataset = torch.load(f"by_dataset/{split}_{name}_{i}.pt")
-                loss_list = loss_collection(model, dataset)
-                loss_dicit[name][split].extend(loss_list)
-pickle.dump(loss_dicit, open("loss_dict.pkl", "wb"))
-plt.figure(figsize=(10, 5))
-fig, axs = plt.subplots(len(loss_dicit), figsize=(10, 5 * len(loss_dicit)))
-axs = np.atleast_2d(axs)
-for ax, (dataset_name, dataset_loss) in zip(axs.flatten(), loss_dicit.items()):
-    for phase_name, phase_loss in dataset_loss.items():
-        weights = np.ones_like(phase_loss) / len(phase_loss)
-        ax.hist(phase_loss, bins=50, label=phase_name, alpha=0.5, weights=weights)
-    ax.set_title(f'Loss values histogram for {dataset_name}')
-    ax.set_xlabel('Loss')
-    ax.set_ylabel('Percentage')
-    ax.legend()
-plt.tight_layout()
-plt.savefig("loss_histograms.png")
-plt.show()
+                loss_list, prob_list, ppl_list = loss_collection(model, dataset)
+                loss_dict[name][split].extend(loss_list)
+                prob_dict[name][split].extend(prob_list)
+                ppl_dict[name][split].extend(ppl_list)
+pickle.dump(loss_dict, open("loss_dict.pkl", "wb"))
+pickle.dump(prob_dict, open("prob_dict.pkl", "wb"))
+pickle.dump(ppl_dict, open("ppl_dict.pkl", "wb"))
+figure_draw(loss_dict, "Loss")
+figure_draw(prob_dict, "Prob")
+figure_draw(ppl_dict, "PPL")
+
 
