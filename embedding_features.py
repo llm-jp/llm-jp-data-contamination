b10879a1d5b77e42616bf2ced82f5ba25f6bb4d9
PososAgapo <bchen@kth.se>
2024-07-16 17:25:40 +0900

    fix bugs

--- embedding_features.py
+++ embedding_features.py
@@ -19,7 +19,7 @@ parser.add_argument("--dataset_name", type=str, default="all", choices=["ArXiv",
 parser.add_argument("--cuda", type=int, default=1, help="cuda device")
 parser.add_argument("--skip_calculation", type=str, default="True")
 parser.add_argument("--reference_model", type=str, default="True")
-parser.add_argument("--samples", type=int, default=1000)
+parser.add_argument("--samples", type=int, default=100)
 parser.add_argument("--gradient_collection", type=str, default=False)
 args = parser.parse_args()
 
@@ -107,7 +107,7 @@ for dataset_name in dataset_names:
     plt.title('PCA of Member and Non-Member Embeddings')
     plt.legend()
     plt.grid(True)
-    plt.savefig(f'embedding_figure/PCA_{dataset_name}_{args.model_size}.png')
+    #plt.savefig(f'embedding_figure/PCA_{dataset_name}_{args.model_size}.png')
     plt.show()
             #attentions = outputs.attentions
             #pdb.set_trace()
