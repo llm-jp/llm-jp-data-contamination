b75d0cfaeea70601fea0ddf24a5cbc47962a20e6
PososAgapo <bchen@kth.se>
2024-07-17 23:11:57 +0900

    fix bugs

--- feature_aggregation.py
+++ feature_aggregation.py
@@ -3,70 +3,74 @@ from sklearn.decomposition import PCA
 from utils import *
 from sklearn.metrics import roc_auc_score, silhouette_score, f1_score, davies_bouldin_score
 
-
-dataset_name = "Pile-CC"
+dataset_names = ["ArXiv", "DM Mathematics",
+                 "FreeLaw", "Github", "HackerNews", "NIH ExPorter",
+                 "Pile-CC", "PubMed Abstracts", "PubMed Central", "StackExchange",
+                 "USPTO Backgrounds", "Wikipedia (en)", "WikiMIA"]
 model_size = "160m"
-loss_dict = pickle.load(open(f"feature_result/{dataset_name}_{model_size}_loss_dict.pkl", "rb"))
-prob_dict = pickle.load(open(f"feature_result/{dataset_name}_{model_size}_prob_dict.pkl", "rb"))
-ppl_dict = pickle.load(open(f"feature_result/{dataset_name}_{model_size}_ppl_dict.pkl", "rb"))
-mink_plus_dict = pickle.load(open(f"feature_result/{dataset_name}_{model_size}_mink_plus_dict.pkl", "rb"))
-zlib_dict = pickle.load(open(f"feature_result/{dataset_name}_{model_size}_zlib_dict.pkl", "rb"))
+for dataset_name in dataset_names:
+    loss_dict = pickle.load(open(f"feature_result/{dataset_name}_{model_size}_loss_dict.pkl", "rb"))
+    prob_dict = pickle.load(open(f"feature_result/{dataset_name}_{model_size}_prob_dict.pkl", "rb"))
+    ppl_dict = pickle.load(open(f"feature_result/{dataset_name}_{model_size}_ppl_dict.pkl", "rb"))
+    mink_plus_dict = pickle.load(open(f"feature_result/{dataset_name}_{model_size}_mink_plus_dict.pkl", "rb"))
+    zlib_dict = pickle.load(open(f"feature_result/{dataset_name}_{model_size}_zlib_dict.pkl", "rb"))
 
-aggregated_train = []
-aggregated_test = []
-aggregated_val = []
-for idx, dict in enumerate([loss_dict, prob_dict, ppl_dict, mink_plus_dict, zlib_dict]):
-    for set_name in ["train", "test"]:
-        data = np.array(dict[dataset_name][set_name])
-        mean1, std1 = np.mean(data), np.std(data)
-        normalized_value = (dict[dataset_name][set_name] - mean1) / std1
-        if set_name == "train":
-            aggregated_train.append(remove_outliers(normalized_value.tolist()))
-        elif set_name == "test":
-            aggregated_test.append(remove_outliers(normalized_value.tolist()))
-        else:
-            aggregated_val.append(normalized_value.tolist())
+    aggregated_train = []
+    aggregated_test = []
+    aggregated_val = []
+    for idx, dict in enumerate([loss_dict, prob_dict, ppl_dict, mink_plus_dict, zlib_dict]):
+        for set_name in ["train", "test"]:
+            data = np.array(dict[dataset_name][set_name])
+            mean1, std1 = np.mean(data), np.std(data)
+            normalized_value = (dict[dataset_name][set_name] - mean1) / std1
+            if set_name == "train":
+                aggregated_train.append(remove_outliers(normalized_value.tolist()))
+            elif set_name == "test":
+                aggregated_test.append(remove_outliers(normalized_value.tolist()))
+            else:
+                aggregated_val.append(normalized_value.tolist())
 
-fig, axs = plt.subplots(2, 1, figsize=(10, 10))
+    fig, axs = plt.subplots(2, 1, figsize=(10, 10))
 
-# 去除离群值
-train_no_outliers = remove_outliers(aggregated_train)
-test_no_outliers = remove_outliers(aggregated_test)
-val_no_outliers = remove_outliers(aggregated_val)
+    # 去除离群值
+    train_no_outliers = remove_outliers(aggregated_train)
+    test_no_outliers = remove_outliers(aggregated_test)
+    val_no_outliers = remove_outliers(aggregated_val)
 
-member_embed_array = np.array(aggregated_train)
-non_member_embed_array = np.array(aggregated_test)
-# Concatenate for PCA
-all_embed_array = np.vstack([member_embed_array, non_member_embed_array])
-labels = np.array([1] * len(member_embed_array) + [0] * len(non_member_embed_array))
-# Perform PCA
-pca = PCA(n_components=2)
-pca_result = pca.fit_transform(all_embed_array)
-# Separate the results
-pca_member_embed = pca_result[labels == 1]
-pca_non_member_embed = pca_result[labels == 0]
-# Plotting
-plt.figure(figsize=(14, 8))
-plt.scatter(pca_member_embed[:, 0], pca_member_embed[:, 1], c='blue', label='Member Text', alpha=0.5)
-plt.scatter(pca_non_member_embed[:, 0], pca_non_member_embed[:, 1], c='red', label='Non-Member Text',
-            alpha=0.5)
-plt.xlabel('PCA Component 1')
-plt.ylabel('PCA Component 2')
-plt.title('PCA of Member and Non-Member Embeddings')
-plt.legend()
-plt.grid(True)
-plt.savefig(f'PCA_output_feature_{dataset_name}_{model_size}.png')
-plt.show()
-labels = np.array([1] * len(member_embed_array) + [0] * len(non_member_embed_array))
-X = np.vstack((member_embed_array, non_member_embed_array))
-db_index = davies_bouldin_score(X, labels)
-silhouette_avg = silhouette_score(X, labels)
-print(db_index)
-print(silhouette_avg)
-# print(f"DB Index : ", db_index)
-# f.write(f"{dataset_name} DB Index : {db_index}\n")
-# print(f"Silhouette Score: ", silhouette_avg)
-# f.write(f"{dataset_name} Silhouette Score : {silhouette_avg}\n")
+    member_embed_array = np.array(aggregated_train).T
+    non_member_embed_array = np.array(aggregated_test).T
+    # Concatenate for PCA
+    all_embed_array = np.vstack([member_embed_array, non_member_embed_array])
+    labels = np.array([1] * len(member_embed_array) + [0] * len(non_member_embed_array))
+    # Perform PCA
+    pca = PCA(n_components=2)
+    pca_result = pca.fit_transform(all_embed_array)
+    # Separate the results
+    pca_member_embed = pca_result[labels == 1]
+    pca_non_member_embed = pca_result[labels == 0]
+    # Plotting
+    plt.figure(figsize=(14, 8))
+    plt.scatter(pca_member_embed[:, 0], pca_member_embed[:, 1], c='blue', label='Member Text', alpha=0.5)
+    plt.scatter(pca_non_member_embed[:, 0], pca_non_member_embed[:, 1], c='red', label='Non-Member Text',
+                alpha=0.5)
+    plt.xlabel('PCA Component 1')
+    plt.ylabel('PCA Component 2')
+    plt.title('PCA of Member and Non-Member Embeddings')
+    plt.legend()
+    plt.grid(True)
+    plt.savefig(f'PCA_output_feature_{dataset_name}_{model_size}.png')
+    plt.show()
+    labels = np.array([1] * len(member_embed_array) + [0] * len(non_member_embed_array))
+    X = np.vstack((member_embed_array, non_member_embed_array))
+    db_index = davies_bouldin_score(X, labels)
+    silhouette_avg = silhouette_score(X, labels)
+    print(db_index)
+    print(silhouette_avg)
+
+    # print(f"DB Index : ", db_index)
+    # f.write(f"{dataset_name} DB Index : {db_index}\n")
+    # print(f"Silhouette Score: ", silhouette_avg)
+    # f.write(f"{dataset_name} Silhouette Score : {silhouette_avg}\n")
 
 
 
