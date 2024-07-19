347ada6b047421e66c83393a474fc62baecb91c5
PososAgapo <bchen@kth.se>
2024-07-19 19:18:18 +0900

    fix bugs

--- memorization_score_filtering.py
+++ memorization_score_filtering.py
@@ -74,7 +74,7 @@ for dataset_name in dataset_names:
                                          min_length=input_length+output_length)
             comparasion_result = generations["sequences"][0][input_length:] == tokenized_inputs["input_ids"][0][input_length:input_length+output_length]
             score = sum(comparasion_result) / (output_length - input_length)
-            score = mem_score.cpu().numpy()
+            score = score.cpu().numpy()
             mem_score = mem_score.append({"set_name": set_name, "batch_idx": idx, "mem_score": score}, ignore_index=True)
     mem_score.to_csv(f"{args.model_size}_{dataset_name}_mem_score.csv")
             #for idx, ratio in enumerate(np.linspace(0, 1, 11)[1:]):
