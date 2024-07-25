be7588e1e14d5674cc99296422fdb4c0784d6bf6
PososAgapo <bchen@kth.se>
2024-07-25 22:11:55 +0900

    add reference loss

--- mix_distribution.py
+++ mix_distribution.py
@@ -21,10 +21,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--batch_size", type=int, default=4)
 parser.add_argument("--max_length", type=int, default=2048)
 parser.add_argument("--model_size", type=str, default="160m")
-parser.add_argument("--dataset_name", type=str, default="Pile-CC", choices=["ArXiv", "DM Mathematics",
-                 "FreeLaw", "Github",  "HackerNews", "NIH ExPorter",
-                "Pile-CC", "PubMed Abstracts", "PubMed Central", "StackExchange",
-                "USPTO Backgrounds", "Wikipedia (en)", "WikiMIA", "all"])
+parser.add_argument("--dataset_name", type=str, default="Pile-CC", choices=["arxiv", "dm_mathematics", "github", "hackernews", "pile_cc",
+                     "pubmed_central", "wikipedia_(en)", "full_pile", "all"])
 parser.add_argument("--cuda", type=int, default=0, help="cuda device")
 parser.add_argument("--refer_cuda", type=int, default=7, help="cuda device")
 parser.add_argument("--skip_calculation", type=str, default="True")
