bffc361f1634c92ebad6d065781947c99ff9b7e5
PososAgapo <bchen@kth.se>
2024-07-29 13:21:54 +0900

    add index distribution analysis

--- memorization_statistics.py
+++ memorization_statistics.py
@@ -10,10 +10,13 @@ for dataset_name in dataset_names:
     nonmember_table = table[table["set_name"] == "nonmember"]
     member_scores = member_table["mem_score"].values
     nonmember_scores = nonmember_table["mem_score"].values
+    bins = np.arange(0, 1.1, 0.1)  # 包含1.0
+
     plt.figure()
-    plt.hist(member_scores, bins=50, alpha=0.5, label='member')
-    plt.hist(nonmember_scores, bins=50, alpha=0.5, label='nonmember')
+    plt.hist(member_scores, bins=bins, alpha=0.5, label='member', edgecolor='black')
+    plt.hist(nonmember_scores, bins=bins, alpha=0.5, label='nonmember', edgecolor='black')
     plt.legend(loc='upper right')
     plt.title(f"{dataset_name} Mem Score Distribution")
+    plt.xticks(bins)  # 设置X轴的刻度
     plt.savefig(f"mem_score_online/{model_size}/{dataset_name}_mem_score.png")
     plt.show()
