85899d222e904c716bca14a0e515e53aa97ac76b
PososAgapo <bchen@kth.se>
2024-08-03 16:18:07 +0900

    fix bugs in generation features

--- sem_score_comparision.py
+++ sem_score_comparision.py
@@ -11,6 +11,7 @@ from datasets import load_dataset
 import copy
 import evaluate
 import pdb
+import pickle
 
 def bleurt_score(predictions, references):
     """Compute the average BLEURT score over the gpt responses
@@ -42,9 +43,8 @@ def rougeL_score(predictions, references):
     return rougeL_score
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--batch_size", type=int, default=1)
+parser.add_argument("--batch_size", type=int, default=5)
 parser.add_argument("--model_size", type=str, default="410m")
-parser.add_argument("--max_length", type=int, default=96)
 parser.add_argument("--dataset_name", type=str, default="all", choices=["arxiv", "dm_mathematics", "github", "hackernews", "pile_cc",
                      "pubmed_central", "wikipedia_(en)", "full_pile","WikiMIA64", "WikiMIA128","WikiMIA256",
                       "WikiMIAall"])
@@ -103,6 +103,7 @@ for input_length in [48]:
                                                                                                       split="none")
         device = f'cuda:{args.cuda}'
         mem_score = pandas.DataFrame(columns=["set_name", "original_idx",  "mem_score"])
+        generation_samples_list = []
         for set_name in ["member", "nonmember"]:
             cleaned_data, orig_indices = clean_dataset(dataset[set_name], dataset_name, online=True)
             for idx, (data_batch, orig_indices_batch) in tqdm(enumerate(batched_data_with_indices(cleaned_data, orig_indices, batch_size=args.batch_size))):
@@ -110,15 +111,12 @@ for input_length in [48]:
                 if idx * args.batch_size > args.samples:
                     break
                 batched_text = [item for item in data_batch]
-                tokenized_inputs = tokenizer(batched_text, return_tensors="pt", truncation=True)
+                tokenized_inputs = tokenizer(batched_text, return_tensors="pt", truncation=True, padding=True)
                 tokenized_inputs = {key: val.to(device) for key, val in tokenized_inputs.items()}
                 #input_length = int(tokenized_inputs["input_ids"].shape[1] * ratio)
                 #output_length = int(tokenized_inputs["input_ids"].shape[1] * (ratio + 0.1))
-                if tokenized_inputs["input_ids"][0].shape[0] < input_length + args.continuation_size:
-                    input_length = tokenized_inputs["input_ids"][0].shape[0] - args.continuation_size
-                    if input_length < 0:
-                        continue
                 temp_results = []
+                full_decoded = []
                 for _ in tqdm(range(args.generation_samples)):
                     generations = model.generate(tokenized_inputs["input_ids"][0][:input_length].unsqueeze(0),
                                                  do_sample=True,
@@ -128,14 +126,18 @@ for input_length in [48]:
                                                  top_p=1,
                                                 )
                     temp_results.append(tokenizer.decode(generations["sequences"][0][input_length:]))
+                    full_decoded.append(tokenizer.decode(generations["sequences"][0]))
                 text_to_compare = tokenizer.decode(tokenized_inputs["input_ids"][0][input_length:])
                 pdb.set_trace()
                 bleurt_scores = bleurt_score(temp_results, [text_to_compare for _ in range(args.generation_samples)])
                 rougeL_scores = rougeL_score(temp_results, [text_to_compare for _ in range(args.generation_samples)])
-                mem_score = mem_score._append({"set_name": set_name, "original_idx": orig_idx[0], "bleurt_score": bleurt_scores, "rougle_scores":rougeL_scores}, ignore_index=True)
+                mem_score = mem_score._append({"set_name": set_name, "original_idx": orig_idx[0],
+                                               "bleurt_score": bleurt_scores, "rougle_scores":rougeL_scores
+                                               }, ignore_index=True)
                 input_length = temp_input_length
+                generation_samples_list.append([batched_text[0], full_decoded, temp_results, text_to_compare])
         os.makedirs(f"sem_mem_score_online/{args.model_size}", exist_ok=True)
         mem_score.to_csv(f"sem_mem_score_online/{args.model_size}/{dataset_name}_{temp_input_length}_sem_mem_score.csv")
-            #for idx, ratio in enumerate(np.linspace(0, 1, 11)[1:]):
+        pickle.dump(generation_samples_list, open(f"sem_mem_score_online/{args.model_size}/{dataset_name}_{temp_input_length}_generation_samples.pkl", "wb"))
 
 
