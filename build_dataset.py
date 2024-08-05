c1f90788e1449105538b299b3e85e83da05b5054
PososAgapo <bchen@kth.se>
2024-08-05 15:25:07 +0900

    fix bugs in generation features

--- build_dataset.py
+++ build_dataset.py
@@ -3,28 +3,40 @@ import os
 import torch
 from collections import defaultdict
 from tqdm import tqdm
-import itertools
-import multiprocessing as mp
 
 
-def process_and_save_chunk(chunk, name, file_counters, items_per_file):
+def process_and_save_dataset(ds, name, items_per_file=250000, batch_size=10000):
+    file_counters = defaultdict(int)
+
     grouped_by_meta = defaultdict(list)
-    for example in chunk:
-        meta_name = example['meta']["pile_set_name"]
-        grouped_by_meta[meta_name].append(example["text"])
+    count = 0
 
-        if len(grouped_by_meta[meta_name]) >= items_per_file:
-            filename = f"/model/pile/by_dataset/{name}_{meta_name}_{file_counters[meta_name]}.pt"
+    while True:
+        batch = list(ds.take(batch_size))
+        if not batch:
+            break
 
-            # Check if the file already exists
-            if not os.path.exists(filename):
-                os.makedirs(os.path.dirname(filename), exist_ok=True)
-                torch.save(grouped_by_meta[meta_name], filename)
+        for example in batch:
+            meta_name = example['meta']["pile_set_name"]
+            grouped_by_meta[meta_name].append(example["text"])
 
-            grouped_by_meta[meta_name].clear()
-            file_counters[meta_name] += 1
+            # When batch size limit is reached
+            if len(grouped_by_meta[meta_name]) >= items_per_file:
+                filename = f"/model/pile/by_dataset/{name}_{meta_name}_{file_counters[meta_name]}.pt"
 
-    # Save any remaining data
+                # Check if the file already exists
+                if not os.path.exists(filename):
+                    os.makedirs(os.path.dirname(filename), exist_ok=True)
+                    torch.save(grouped_by_meta[meta_name], filename)
+
+                # Reset current group
+                grouped_by_meta[meta_name].clear()
+                file_counters[meta_name] += 1
+
+        count += len(batch)
+        print(f"Processed {count} examples")
+
+    # Save remaining data
     for meta, dataset in grouped_by_meta.items():
         if dataset:  # Save if not empty
             filename = f"/model/pile/by_dataset/{name}_{meta}_{file_counters[meta]}.pt"
@@ -33,33 +45,10 @@ def process_and_save_chunk(chunk, name, file_counters, items_per_file):
                 torch.save(dataset, filename)
 
 
-def process_and_save_dataset(ds, name, items_per_file=250000, batch_size=10000, num_workers=4):
-    file_counters = defaultdict(int)
-
-    chunk = []
-    pool = mp.Pool(num_workers)
-
-    for example in tqdm(itertools.islice(ds, 0, None)):
-        chunk.append(example)
-
-        if len(chunk) >= batch_size:
-            pool.apply_async(process_and_save_chunk, (chunk, name, file_counters, items_per_file))
-            chunk = []
-
-    if chunk:
-        pool.apply_async(process_and_save_chunk, (chunk, name, file_counters, items_per_file))
-
-    pool.close()
-    pool.join()
-
-
 ds_valid = load_dataset("monology/pile-uncopyrighted", cache_dir="/model/pile", split="validation", streaming=True)
 ds_test = load_dataset("monology/pile-uncopyrighted", cache_dir="/model/pile", split="test", streaming=True)
 ds_train = load_dataset("monology/pile-uncopyrighted", cache_dir="/model/pile", split="train", streaming=True)
 
-print("Processing and saving valid datasets")
-process_and_save_dataset(ds_valid, "valid", items_per_file=100000000, batch_size=10000, num_workers=4)
-print("Processing and saving test datasets")
-process_and_save_dataset(ds_test, "test", items_per_file=100000000, batch_size=10000, num_workers=4)
-print("Processing and saving train datasets")
-process_and_save_dataset(ds_train, "train", items_per_file=250000, batch_size=10000, num_workers=4)
+process_and_save_dataset(ds_valid, "valid", items_per_file=250000, batch_size=10000)
+process_and_save_dataset(ds_test, "test", items_per_file=250000, batch_size=10000)
+process_and_save_dataset(ds_train, "train", items_per_file=250000, batch_size=10000)
