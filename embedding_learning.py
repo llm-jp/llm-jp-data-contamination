9742f1aa7e9a69ab23952007ab2e1ee95cf8f5b1
PososAgapo <bchen@kth.se>
2024-08-06 22:06:49 +0900

    fix bugs in generation features

--- embedding_learning.py
+++ embedding_learning.py
@@ -16,7 +16,8 @@ import torch.optim as optim
 import numpy as np
 from torch.utils.data import DataLoader, TensorDataset
 
-
+layer_results = pd.DataFrame(columns=["Dataset", "Layer", "Train Accuracy", "Test Accuracy"])
+layer_results_path = "layer_results.csv"
 def pad_embeddings(embed_list, attn_mask_list, max_length):
     padded_embed_list = []
     attention_masks = []
@@ -121,6 +122,8 @@ model.generation_config.output_hidden_states = True
 model.generation_config.output_scores = True
 model.generation_config.return_dict_in_generate = True
 
+layer_num = model.config.num_hidden_layers
+
 for dataset_name in dataset_names:
     if "WikiMIA" in dataset_name:
         dataset = form_dataset(dataset_name)
@@ -137,138 +140,131 @@ for dataset_name in dataset_names:
     non_member_embed_list = []
     member_attn_mask_list = []
     nonmember_attn_mask_list = []
-    for set_name in ["member", "nonmember"]:
-        cleaned_data, orig_indices = clean_dataset(dataset[set_name], dataset_name, online=True)
-        for idx, (data_batch, orig_indices_batch) in tqdm(enumerate(batched_data_with_indices(cleaned_data, orig_indices, batch_size=args.batch_size))):
-            if idx * args.batch_size > args.samples:
-                break
-            batched_text = [item for item in data_batch]
-            tokenized_inputs = tokenizer(batched_text,
-                                         return_tensors="pt",
-                                         truncation=True,
-                                         padding=True,
-                                         max_length=2048,
-                                         )
-            tokenized_inputs = {key: val.to(device) for key, val in tokenized_inputs.items()}
-            target_labels = tokenized_inputs["input_ids"].clone().to(device)
-            target_labels[tokenized_inputs["attention_mask"] == 0] = -100
+    for layer_index in range(layer_num):
+        for set_name in ["member", "nonmember"]:
+            cleaned_data, orig_indices = clean_dataset(dataset[set_name], dataset_name, online=True)
+            for idx, (data_batch, orig_indices_batch) in tqdm(enumerate(batched_data_with_indices(cleaned_data, orig_indices, batch_size=args.batch_size))):
+                if idx * args.batch_size > args.samples:
+                    break
+                batched_text = [item for item in data_batch]
+                tokenized_inputs = tokenizer(batched_text,
+                                             return_tensors="pt",
+                                             truncation=True,
+                                             padding=True,
+                                             max_length=2048,
+                                             )
+                tokenized_inputs = {key: val.to(device) for key, val in tokenized_inputs.items()}
+                target_labels = tokenized_inputs["input_ids"].clone().to(device)
+                target_labels[tokenized_inputs["attention_mask"] == 0] = -100
+                with torch.no_grad():
+                    outputs = model(**tokenized_inputs, labels=target_labels, output_hidden_states=True, return_dict=True)
+                hidden_states = outputs.hidden_states
+                context_embedding = hidden_states[-2]
+                layer_num = len(hidden_states)
+                if set_name == "member":
+                    member_embed_list.append(context_embedding.cpu())
+                    member_attn_mask_list.append(tokenized_inputs["attention_mask"].cpu())
+                elif set_name == "nonmember":
+                    non_member_embed_list.append(context_embedding.cpu())
+                    nonmember_attn_mask_list.append(tokenized_inputs["attention_mask"].cpu())
+        max_length = max(max(embed.shape[1] for embed in member_embed_list),
+                         max(embed.shape[1] for embed in non_member_embed_list))
+
+        # 填充并合并embedding
+        member_embeddings, member_attn_masks = pad_embeddings(member_embed_list, member_attn_mask_list, max_length)
+        nonmember_embeddings, nonmember_attn_masks = pad_embeddings(non_member_embed_list, nonmember_attn_mask_list, max_length)
+
+        # 创建标签
+        member_labels = torch.ones(member_embeddings.shape[0])
+        nonmember_labels = torch.zeros(nonmember_embeddings.shape[0])
+
+        # 合并数据和标签
+        X = torch.cat([member_embeddings, nonmember_embeddings], axis=0)
+        y = torch.cat([member_labels, nonmember_labels], axis=0)
+        attention_masks = torch.cat([member_attn_masks, nonmember_attn_masks], dim=0)
+
+        # 将数据分为训练集和测试集
+        X_train, X_test, y_train, y_test, attn_train, attn_test = train_test_split(X, y, attention_masks, test_size=0.2, random_state=42)
+
+        # 转换数据为tensor
+        X_train = torch.tensor(X_train, dtype=torch.float32).view(-1, member_embeddings.shape[1], member_embeddings.shape[2])
+        X_test = torch.tensor(X_test, dtype=torch.float32).view(-1, member_embeddings.shape[1], member_embeddings.shape[2])
+        y_train = torch.tensor(y_train, dtype=torch.long)
+        y_test = torch.tensor(y_test, dtype=torch.long)
+        attn_train = torch.tensor(attn_train, dtype=torch.float32)
+        attn_test = torch.tensor(attn_test, dtype=torch.float32)
+
+        batch_size = 4  # 可根据需要调整
+        train_dataset = TensorDataset(X_train, y_train, attn_train)
+        test_dataset = TensorDataset(X_test, y_test, attn_test)
+        train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+        test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+        input_dim = member_embeddings.shape[2] # 输入的特征维度
+        hidden_dim = 256
+        output_dim = 2
+        num_layers = 4
+        num_heads = 8
+
+        pred_model = TransformerClassifier(input_dim, hidden_dim, output_dim, num_layers, num_heads)
+
+
+        # 使用GPU
+        device = f'cuda:{args.cuda}'
+        pred_model = pred_model.to(device)
+        X_train, X_test, y_train, y_test = X_train.to(device), X_test.to(device), y_train.to(device), y_test.to(device)
+
+        # 定义损失和优化器
+        criterion = nn.CrossEntropyLoss()
+        optimizer = optim.Adam(pred_model.parameters(), lr=0.0001)
+        # 训练模型
+        num_epochs = 10
+        for epoch in range(num_epochs):
+            pred_model.train()
+            for i, (inputs, labels, attention_masks) in enumerate(train_loader):
+                inputs, labels, attention_masks = inputs.to(device), labels.to(device), attention_masks.to(device)
+                optimizer.zero_grad()
+                outputs = pred_model(inputs, attention_masks)
+                loss = criterion(outputs, labels)
+                loss.backward()
+                optimizer.step()
+                if (i + 1) % 10 == 0:  # 每10个批次打印一次loss
+                    print(f'Epoch [{epoch + 1}/{num_epochs}], Step [{i + 1}/{len(train_loader)}], Loss: {loss.item():.4f}')
+            pred_model.eval()
+            all_preds = []
+            all_labels = []
             with torch.no_grad():
-                outputs = model(**tokenized_inputs, labels=target_labels, output_hidden_states=True, return_dict=True)
-            hidden_states = outputs.hidden_states
-            context_embedding = hidden_states[-2]
-            if set_name == "member":
-                member_embed_list.append(context_embedding.cpu())
-                member_attn_mask_list.append(tokenized_inputs["attention_mask"].cpu())
-            elif set_name == "nonmember":
-                non_member_embed_list.append(context_embedding.cpu())
-                nonmember_attn_mask_list.append(tokenized_inputs["attention_mask"].cpu())
-del model
-max_length = max(max(embed.shape[1] for embed in member_embed_list),
-                 max(embed.shape[1] for embed in non_member_embed_list))
-
-# 填充并合并embedding
-member_embeddings, member_attn_masks = pad_embeddings(member_embed_list, member_attn_mask_list, max_length)
-nonmember_embeddings, nonmember_attn_masks = pad_embeddings(non_member_embed_list, nonmember_attn_mask_list, max_length)
-
-# 创建标签
-member_labels = torch.ones(member_embeddings.shape[0])
-nonmember_labels = torch.zeros(nonmember_embeddings.shape[0])
-
-# 合并数据和标签
-X = torch.cat([member_embeddings, nonmember_embeddings], axis=0)
-y = torch.cat([member_labels, nonmember_labels], axis=0)
-attention_masks = torch.cat([member_attn_masks, nonmember_attn_masks], dim=0)
-
-# 将数据分为训练集和测试集
-X_train, X_test, y_train, y_test, attn_train, attn_test = train_test_split(X, y, attention_masks, test_size=0.2, random_state=42)
-
-# 转换数据为tensor
-X_train = torch.tensor(X_train, dtype=torch.float32).view(-1, member_embeddings.shape[1], member_embeddings.shape[2])
-X_test = torch.tensor(X_test, dtype=torch.float32).view(-1, member_embeddings.shape[1], member_embeddings.shape[2])
-y_train = torch.tensor(y_train, dtype=torch.long)
-y_test = torch.tensor(y_test, dtype=torch.long)
-attn_train = torch.tensor(attn_train, dtype=torch.float32)
-attn_test = torch.tensor(attn_test, dtype=torch.float32)
-
-batch_size = 4  # 可根据需要调整
-train_dataset = TensorDataset(X_train, y_train, attn_train)
-test_dataset = TensorDataset(X_test, y_test, attn_test)
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
-
-# 定义二元分类模型
+                for inputs, labels, attention_masks in train_loader:
+                    inputs, labels, attention_masks = inputs.to(device), labels.to(device), attention_masks.to(device)
+                    outputs = model(inputs, attention_masks)
+                    _, predicted = torch.max(outputs.data, 1)
+                    all_preds.extend(predicted.cpu().numpy())
+                    all_labels.extend(labels.cpu().numpy())
+            train_accuracy = accuracy_score(all_labels, all_preds)
+            print(f'Train Accuracy: {accuracy:.4f}')
+            print(classification_report(all_labels, all_preds, target_names=['Nonmember', 'Member']))
 
 
-# 模型的超参数
-# input_dim = member_embeddings.shape[2]
-# hidden_dim = member_embeddings.shape[2]  # 可以根据需要调整
-# output_dim = 2
-# num_layers = 4
-# num_heads = 4
-#
-# model = TransformerClassifier(input_dim, hidden_dim, output_dim, num_layers, num_heads)
-
-input_dim = member_embeddings.shape[2] # 输入的特征维度
-hidden_dim = 256
-output_dim = 2
-num_layers = 4
-num_heads = 8
-
-model = TransformerClassifier(input_dim, hidden_dim, output_dim, num_layers, num_heads)
-
-
-# 使用GPU
-device = f'cuda:{args.cuda}'
-model = model.to(device)
-X_train, X_test, y_train, y_test = X_train.to(device), X_test.to(device), y_train.to(device), y_test.to(device)
-
-# 定义损失和优化器
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.0001)
-
-# 训练模型
-num_epochs = 10
-for epoch in range(num_epochs):
-    model.train()
-    for i, (inputs, labels, attention_masks) in enumerate(train_loader):
-        inputs, labels, attention_masks = inputs.to(device), labels.to(device), attention_masks.to(device)
-        optimizer.zero_grad()
-        outputs = model(inputs, attention_masks)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-        if (i + 1) % 10 == 0:  # 每10个批次打印一次loss
-            print(f'Epoch [{epoch + 1}/{num_epochs}], Step [{i + 1}/{len(train_loader)}], Loss: {loss.item():.4f}')
-    model.eval()
-    all_preds = []
-    all_labels = []
-    with torch.no_grad():
-        for inputs, labels, attention_masks in train_loader:
-            inputs, labels, attention_masks = inputs.to(device), labels.to(device), attention_masks.to(device)
-            outputs = model(inputs, attention_masks)
-            _, predicted = torch.max(outputs.data, 1)
-            all_preds.extend(predicted.cpu().numpy())
-            all_labels.extend(labels.cpu().numpy())
-    accuracy = accuracy_score(all_labels, all_preds)
-    print(f'Train Accuracy: {accuracy:.4f}')
-    print(classification_report(all_labels, all_preds, target_names=['Nonmember', 'Member']))
-
-
-#Test Accuracy: 0.5450 model size 2.8 arxiv
-#Test Accuracy: 0.5341 model size 2.8 hackernews
-# 评估模型
-model.eval()
-all_preds = []
-all_labels = []
-with torch.no_grad():
-    for inputs, labels, attention_masks in test_loader:
-        inputs, labels, attention_masks = inputs.to(device), labels.to(device), attention_masks.to(device)
-        outputs = model(inputs, attention_masks)
-        _, predicted = torch.max(outputs.data, 1)
-        all_preds.extend(predicted.cpu().numpy())
-        all_labels.extend(labels.cpu().numpy())
-accuracy = accuracy_score(all_labels, all_preds)
-print(f'Test Accuracy: {accuracy:.4f}')
-print(classification_report(all_labels, all_preds, target_names=['Nonmember', 'Member']))
-
+        #Test Accuracy: 0.5450 model size 2.8 arxiv
+        #Test Accuracy: 0.5341 model size 2.8 hackernews
+        # 评估模型
+        pred_model.eval()
+        all_preds = []
+        all_labels = []
+        with torch.no_grad():
+            for inputs, labels, attention_masks in test_loader:
+                inputs, labels, attention_masks = inputs.to(device), labels.to(device), attention_masks.to(device)
+                outputs = pred_model(inputs, attention_masks)
+                _, predicted = torch.max(outputs.data, 1)
+                all_preds.extend(predicted.cpu().numpy())
+                all_labels.extend(labels.cpu().numpy())
+        test_accuracy = accuracy_score(all_labels, all_preds)
+        print(f'Test Accuracy: {test_accuracy:.4f}')
+        print(classification_report(all_labels, all_preds, target_names=['Nonmember', 'Member']))
+        layer_results = layer_results._append({
+            "Dataset": dataset_name,
+            "Layer": layer_index,
+            "Train Accuracy": train_accuracy,
+            "Test Accuracy": test_accuracy},
+            ignore_index=True)
 
