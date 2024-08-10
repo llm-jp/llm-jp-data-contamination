981b91e68dc22febb6e5267bfd8c7a66f539d1b3
PososAgapo <bchen@kth.se>
2024-08-10 14:12:55 +0900

    fix bugs in generation features

--- data_random_sample.py
+++ data_random_sample.py
@@ -27,6 +27,7 @@ for dataset_name in ["ArXiv", "Pile-CC", "Github", "StackExchange", "Wikipedia(e
                 batch = data[i:i+batch_size]
                 texts = [item for item in batch]
                 tokenized_batch = tokenizer(texts, truncation=True, padding='longest', return_tensors="pt", max_length=2048)
+                pdb.set_trace()
                 # use attention_mask to obtain the length of each text
                 lengths = tokenized_batch['attention_mask'].sum(dim=1).cuda(1)
                 valid_indices = (lengths >= min_length) & (lengths <= max_length)
