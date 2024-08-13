dd37ceece6239195e8f3a0112ecfaee299bb47e6
PososAgapo <bchen@kth.se>
2024-08-13 19:22:55 +0900

    fix bugs in generation features

--- data_random_sample.py
+++ data_random_sample.py
@@ -1,18 +1,16 @@
 import os
-import random
-import torch
+import pdb
 from tqdm import tqdm
+import torch
+import random
 from transformers import AutoTokenizer
-from datasets import Dataset, DatasetDict
-from concurrent.futures import ThreadPoolExecutor
+from datasets import DatasetDict, Dataset
 
 random.seed(42)
 
-
-def load_test_data(test_folder, test_files, min_length, max_length, sample_size, filter_test, tokenizer, batch_size, dataset_name):
-    test_data, test_dataset = [], []
+def load_test_data(test_folder, test_files, min_length, max_length, sample_size, filter_test, tokenizer, batch_size):
     if len(test_files) > 1:
-        sampled_test_files = random.sample(test_files, len(test_files))
+        sampled_test_files = random.sample(test_files, num_samples)
         if filter_test:
             test_data = load_and_filter_data(sampled_test_files, test_folder, min_length, max_length, sample_size,
                                              tokenizer, batch_size)
@@ -38,10 +36,12 @@ def filter_data(data, min_length, max_length, tokenizer, batch_size):
     filtered_data = []
     for i in tqdm(range(0, len(data), batch_size)):
         batch = data[i:i + batch_size]
-        texts = batch
+        texts = [item for item in batch]
         tokenized_batch = tokenizer(texts, truncation=True, padding=True, return_tensors="pt", max_length=2048).to(
             "cuda:1")
-        lengths = tokenized_batch['attention_mask'].sum(dim=1)
+        # pdb.set_trace()
+        # use attention_mask to obtain the length of each text
+        lengths = tokenized_batch['attention_mask'].cuda(1).sum(dim=1)
         valid_indices = (lengths >= min_length) & (lengths <= max_length)
         filtered_data.extend([batch[j] for j in range(len(batch)) if valid_indices[j]])
     return filtered_data
@@ -50,80 +50,56 @@ def filter_data(data, min_length, max_length, tokenizer, batch_size):
 def load_and_filter_data(files, folder, min_length, max_length, sample_size, tokenizer, batch_size):
     """filtering and load"""
     merged_data = []
-    with ThreadPoolExecutor() as executor:
-        futures = [
-            executor.submit(load_and_filter_file, os.path.join(folder, file), min_length, max_length, tokenizer,
-                            batch_size) for file in files]
-        for future in futures:
-            filtered_data = future.result()
-            merged_data.extend(filtered_data)
-            if len(merged_data) > sample_size:
-                return random.sample(merged_data, sample_size)
+    for file in files:
+        dataset = torch.load(os.path.join(folder, file))
+        filtered_data = filter_data(dataset, min_length, max_length, tokenizer, batch_size)
+        merged_data.extend(filtered_data)
+    if len(merged_data) > sample_size:
+        return random.sample(merged_data, sample_size)
     return merged_data
-
-
-def load_and_filter_file(filepath, min_length, max_length, tokenizer, batch_size):
-    dataset = torch.load(filepath)
-    return filter_data(dataset, min_length, max_length, tokenizer, batch_size)
-
-
-# 处理数据集名称和长度限制
-dataset_names = ["ArXiv", "Wikipedia (en)", "PubMed Abstracts", "USPTO Backgrounds",
-                 "FreeLaw", "PubMed Central", "Enron Emails", "HackerNews",
-                 "NIH", "DM Mathematics", "Ubuntu IRC", "EuroParl", "PhilPapers", "Gutenberg (PG-19)"]
-
-min_lengths = [50, 150, 250, 350, 450, 550, 650, 750, 850, 950]
-
-# 打开缓存字典进行共享tokenizer
-tokenizer = AutoTokenizer.from_pretrained(
-    "EleutherAI/pythia-12b-deduped", revision="step143000", cache_dir="./pythia-12b-deduped/step143000"
-)
-tokenizer.pad_token = tokenizer.eos_token
-
-
-# 创建并行处理函数
-def process_dataset(dataset_name):
+a =["ArXiv","Wikipedia (en)", "PubMed Abstracts", "USPTO Backgrounds", "FreeLaw"]
+b = ["PubMed Central", "Enron Emails", "HackerNews", "NIH", "DM Mathematics"]
+c = ["Ubuntu IRC", "EuroParl", "PhilPapers", "Gutenberg (PG-19)"]
+for dataset_name in a:
     train_folder = "/model/pile/by_dataset/"
     test_folder = "/model/pile/by_dataset/"
-    print(dataset_name)
-    for min_length in min_lengths:
-        max_length = min_length + 100
-        batch_size = 100
+    tokenizer = AutoTokenizer.from_pretrained(
+        f"EleutherAI/pythia-12b-deduped",
+        revision="step143000",
+        cache_dir=f"./pythia-12b-deduped/step143000",
+    )
+    tokenizer.pad_token = tokenizer.eos_token
+    for min_length in [50, 150, 250, 350, 450, 550, 650, 750, 850, 950]:
+        max_length = min_length + 100  # token
+        batch_size = 100 #deocder batch size
 
-        # 处理文件列表和采样
+        # Step 1: see how many data files exist
         train_files = [f for f in os.listdir(train_folder) if f.startswith(f"train_{dataset_name}_")]
         test_files = [f for f in os.listdir(test_folder) if f.startswith(f"test_{dataset_name}_")]
 
-        num_samples = 5 if len(train_files) > 5 else len(train_files)
+        # Step 2: sample train_{dataset_name}_x
+        num_samples = 1 #5 if len(train_files) > 5 else len(train_files)
         sampled_train_files = random.sample(train_files, num_samples)
 
-        # 过滤和加载数据
-        train_data = load_and_filter_data(sampled_train_files, train_folder, min_length, max_length, 20000, tokenizer,
-                                          batch_size)
+        # Step 3 & 4: merge data and take 20000 samples
+        train_data = load_and_filter_data(sampled_train_files, train_folder, min_length, max_length, 20000, tokenizer, batch_size)
+
+        # whether to filter the test data since the test data is rare and may not reach 20000 sample size
         filter_test = True
-        test_data, test_dataset = load_test_data(test_folder, test_files, min_length, max_length, 20000, filter_test,
-                                                 tokenizer, batch_size, dataset_name)
+        test_data, test_dataset = load_test_data(test_folder, test_files, min_length, max_length, 20000, filter_test, tokenizer, batch_size)
 
-        # 构建 Dataset 和 DatasetDict
-        train_dataset = Dataset.from_dict({"text": train_data})
-        test_dataset_short = Dataset.from_dict({"text": test_data})
-        full_test_dataset = Dataset.from_dict({"text": test_dataset})
-
+        # create data dict
+        train_dataset = Dataset.from_dict({"data": train_data})
+        test_dataset_short = Dataset.from_dict({"data": test_data})
+        full_test_dataset = Dataset.from_dict({"data": test_dataset})
+        # 创建 DatasetDict 对象
         dataset = DatasetDict({
             'member': train_dataset,
             'nonmember': test_dataset_short,
             "full_nonmember": full_test_dataset
         })
 
-        # 保存 DatasetDict
-        save_path = f"./filtered_dataset/{min_length}_{max_length}/{dataset_name}"
-        os.makedirs(save_path, exist_ok=True)
-        dataset.save_to_disk(save_path)
+        # save the dataset
+        os.makedirs(f"./filtered_dataset/{min_length}_{max_length}/{dataset_name}", exist_ok=True)
+        dataset.save_to_disk(f"./filtered_dataset/{min_length}_{max_length}/{dataset_name}")
         print(dataset)
-
-
-# 多线程处理所有数据集
-with ThreadPoolExecutor(max_workers=2) as executor:
-    futures = [executor.submit(process_dataset, dataset_name) for dataset_name in dataset_names]
-    for future in futures:
-        future.result()
