248262707282885ce503520504344eed2a45290f
PososAgapo <bchen@kth.se>
2024-08-13 16:24:40 +0900

    fix bugs in generation features

--- embedding_features.py
+++ embedding_features.py
@@ -1,3 +1,4 @@
+import os
 import pdb
 from transformers import GPTNeoXForCausalLM, AutoTokenizer,  AutoModelForCausalLM,  LogitsProcessorList, MinLengthLogitsProcessor, StoppingCriteriaList,  MaxLengthCriteria
 from utils import form_dataset, batched_data_with_indices, clean_dataset, load_model_and_tokenizer, get_dataset_list, obtain_dataset
@@ -9,28 +10,26 @@ from sklearn.decomposition import PCA
 import numpy as np
 from sklearn.metrics import roc_auc_score, silhouette_score, davies_bouldin_score, calinski_harabasz_score
 import pandas as pd
+from datasets import load_dataset
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--batch_size", type=int, default=1)
 parser.add_argument("--model_size", type=str, default="1b")
-parser.add_argument("--dataset_name", type=str, default="all", choices=["ArXiv", "DM Mathematics",
-                 "FreeLaw", "Github",  "HackerNews", "NIH ExPorter",
-                "Pile-CC", "PubMed Abstracts", "PubMed Central", "StackExchange",
-                "USPTO Backgrounds", "Wikipedia (en)", "WikiMIA", "all"])
+parser.add_argument("--dataset_name", type=str, default="Pile-CC", choices=["arxiv", "dm_mathematics", "github", "hackernews", "pile_cc",
+                     "pubmed_central", "wikipedia_(en)", "full_pile", "temporalarxiv","all"])
 parser.add_argument("--cuda", type=int, default=1, help="cuda device")
 parser.add_argument("--skip_calculation", type=str, default="True")
 parser.add_argument("--samples", type=int, default=1000)
 parser.add_argument("--gradient_collection", type=str, default=False)
 args = parser.parse_args()
 
+
 dataset_names = get_dataset_list(args.dataset_name)
-skip_calculation = False
 model, tokenizer = load_model_and_tokenizer(args)
 tokenizer.pad_token = tokenizer.eos_token
 model.generation_config.pad_token_id = model.generation_config.eos_token_id
-model.generation_config.output_hidden_states = True
-model.generation_config.return_dict_in_generate = True
-
+os.makedirs(f"embedding_results_online/{args.model_size}", exist_ok=True)
+csv_file_path = f"embedding_results_online/{args.model_size}_embedding_result.csv"
 results_df = pd.DataFrame(
     columns=['Dataset Name', 'Layer Index', 'DB Index',
              'Silhouette Score', 'Calinski Harabasz Index'])
@@ -39,8 +38,13 @@ for dataset_name in dataset_names:
     device = f'cuda:{args.cuda}'
     member_embed_list = {}
     non_member_embed_list = {}
-    for set_name in ["train", "test"]:
-        cleaned_data, orig_indices = clean_dataset(dataset[set_name], dataset_name)
+    for set_name in ["member", "nonmember"]:
+        cleaned_data, orig_indices = clean_dataset(dataset[set_name], dataset_name, online=True)
+        if os.path.exists(csv_file_path):
+            layer_results = pd.read_csv(csv_file_path)
+            if ((layer_results["Dataset Name"] == dataset_name) & (layer_results["Layer Index"] == layer_index)).any():
+                print(f"Skipping training for {dataset_name} at layer {layer_index} as previous results are found.")
+                continue
         for idx, (data_batch, orig_indices_batch) in tqdm(enumerate(batched_data_with_indices(cleaned_data, orig_indices, batch_size=args.batch_size))):
             if idx * args.batch_size > args.samples:
                 break
@@ -56,21 +60,18 @@ for dataset_name in dataset_names:
             target_labels[tokenized_inputs["attention_mask"] == 0] = -100
             #pdb.set_trace()
             torch.cuda.synchronize()
-            try:
-                with torch.no_grad():
-                    outputs = model(**tokenized_inputs, labels=target_labels, output_hidden_states=True, return_dict=True)
-                hidden_states = outputs.hidden_states
-                for layer_index in range(len(hidden_states)):
-                    if layer_index not in member_embed_list:
-                        member_embed_list[layer_index] = []
-                        non_member_embed_list[layer_index] = []
-                    context_embedding = hidden_states[layer_index][0].mean(0).squeeze()
-                    if set_name == "train" and len(member_embed_list) < args.samples:
-                        member_embed_list[layer_index].append(context_embedding.cpu())
-                    elif set_name == "test" and len(non_member_embed_list) < args.samples:
-                        non_member_embed_list[layer_index].append(context_embedding.cpu())
-            except:
-                continue
+            with torch.no_grad():
+                outputs = model(**tokenized_inputs, labels=target_labels, output_hidden_states=True, return_dict=True)
+            hidden_states = outputs.hidden_states
+            for layer_index in range(len(hidden_states)):
+                if layer_index not in member_embed_list:
+                    member_embed_list[layer_index] = []
+                    non_member_embed_list[layer_index] = []
+                context_embedding = hidden_states[layer_index][0].mean(0).squeeze()
+                if set_name == "member" and len(member_embed_list) < args.samples:
+                    member_embed_list[layer_index].append(context_embedding.cpu())
+                elif set_name == "nonmember" and len(non_member_embed_list) < args.samples:
+                    non_member_embed_list[layer_index].append(context_embedding.cpu())
     for layer_index in range(len(hidden_states)):
         member_embed_array = torch.stack(member_embed_list[layer_index])
         non_member_embed_array = torch.stack(non_member_embed_list[layer_index])
@@ -106,6 +107,6 @@ for dataset_name in dataset_names:
                                         'Silhouette Score': silhouette_avg,
                                         'Calinski Harabasz Index': calinski_index},
                                        ignore_index=True)
-results_df.to_csv(f"embedding_results/{args.model_size}_embedding_result.csv", index=False)
+        results_df.to_csv(f"embedding_results_online/{args.model_size}_embedding_result.csv", index=False)
 
 
