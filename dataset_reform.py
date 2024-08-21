7ac52d8eac5576d2c3c7971baf630173e92fffca
PososAgapo <bchen@kth.se>
2024-08-21 16:37:51 +0900

    fix bugs in generation features

--- dataset_reform.py
+++ dataset_reform.py
@@ -43,12 +43,12 @@ for dataset_name in dataset_names:
             dataset_indicator = False
     if dataset_indicator:
         print("dataset is good")
-        os.makedirs(f"./mia_dataset_filtered/{dataset_name}_truncated", exist_ok=True)
+        os.makedirs(f"./mia_dataset_filtered/{dataset_name}", exist_ok=True)
         for min_len in [0, 100, 200, 300, 400, 500, 600, 700, 800, 900]:
             #args.min_len = min_len
             #dataset = obtain_dataset(dataset_name, args)
             added_address = args.truncated
-            min_len = args.min_len if args.min_len != 0 else 5
-            max_len = 100 if args.min_len == 0 else min_len + 100
+            min_len = min_len if min_len != 0 else 5
+            max_len = 100 if min_len == 0 else min_len + 100
             dataset = datasets.load_from_disk(f"./{args.load_dir}/{min_len}_{max_len}_{added_address}/{dataset_name}")
-            dataset.save_to_disk(f"./mia_dataset_filtered/{dataset_name}_truncated/{min_len}")
+            dataset.save_to_disk(f"./mia_dataset_filtered/{dataset_name}/{min_len}_{max_len}_truncated")
