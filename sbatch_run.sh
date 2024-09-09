c641f170771ef3ed448badb26dfb26ce2ecdc2df
PososAgapo <bchen@kth.se>
2024-09-09 14:51:19 +0900

    fix bugs in generation features

--- sbatch_run.sh
+++ sbatch_run.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
-#SBATCH --job-name=DataContamination6.9b
+#SBATCH --job-name=DataContamination12b
 #SBATCH --partition=gpu-small
 #SBATCH --gres=gpu:2
 #SBATCH --nodes=1
+#SBATCH --ntasks-per-node=2
 #SBATCH --time=72:00:00
-#SBATCH --output=6.9bj.log
-#SBATCH --error=6.9bj.err
+#SBATCH --output=trunc12bj.log
+#SBATCH --error=trunc12bj.err
 #SBATCH --cpus-per-task=8
 
 
 # Run your script
 srun python runing_both.py \
   --relative absolute \
-  --model_size 6.9b \
-  --truncated untruncated \
+  --model_size 12b \
+  --truncated truncated \
   --dataset_name local_all \
   --min_len 0 \
   --cuda 0 \
   --refer_cuda 1 \
   --dataset_idx 1 \
-  --batch_size 2
+  --batch_size 1
