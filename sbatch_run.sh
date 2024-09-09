b0b3d6909b7f1dc35198a00383cd6bfb5676d26e
PososAgapo <bchen@kth.se>
2024-09-09 14:57:53 +0900

    fix bugs in generation features

--- sbatch_run.sh
+++ sbatch_run.sh
@@ -1,23 +1,43 @@
 #!/bin/bash
-#SBATCH --job-name=DataContamination12b
+#SBATCH --job-name=DataContaminationall
 #SBATCH --partition=gpu-small
-#SBATCH --gres=gpu:2
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=2
+#SBATCH --gres=gpu:8              # 请求两个GPU (total out of 2 nodes which creates 4 tasks)
+#SBATCH --nodes=1                 # 只需要一个节点
 #SBATCH --time=72:00:00
-#SBATCH --output=trunc12bj.log
-#SBATCH --error=trunc12bj.err
-#SBATCH --cpus-per-task=8
+#SBATCH --output=runall
+#SBATCH --error=runall
+#SBATCH --ntasks=4                # 总共运行四个任务
+#SBATCH --cpus-per-task=4         # 每个任务分配4个CPU
 
+# 创建日志目录
 
-# Run your script
-srun python runing_both.py \
-  --relative absolute \
-  --model_size 12b \
-  --truncated truncated \
-  --dataset_name local_all \
-  --min_len 0 \
-  --cuda 0 \
-  --refer_cuda 1 \
-  --dataset_idx 1 \
-  --batch_size 1
+# 出错时退出脚本
+set -e
+
+# 实际运行的命令
+srun_parallel () {
+    local model_size=$1
+    local truncated=$2
+    local cuda_id=$3
+    local refer_cuda_id=$4
+
+    python runing_both.py \
+      --relative absolute \
+      --model_size $model_size \
+      --truncated $truncated \
+      --dataset_name local_all \
+      --min_len 0 \
+      --cuda $cuda_id \
+      --refer_cuda $refer_cuda_id \
+      --dataset_idx 1 \
+      --batch_size 1
+}
+
+# 启动并行任务
+srun --ntasks=1 bash -c "srun_parallel 12b truncated 0 7" &
+srun --ntasks=1 bash -c "srun_parallel 12b untruncated 1 6" &
+srun --ntasks=1 bash -c "srun_parallel 6.9b truncated 2 5" &
+srun --ntasks=1 bash -c "srun_parallel 6.9b untruncated 3 4" &
+
+# 等待所有任务结束
+wait
