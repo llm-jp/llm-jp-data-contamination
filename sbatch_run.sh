fa2c53de570053a477a5a2d444bde071cafc1dd7
PososAgapo <bchen@kth.se>
2024-09-09 19:42:21 +0900

    fix bugs in generation features

--- sbatch_run.sh
+++ sbatch_run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=DataContaminationall
+#SBATCH --job-name=DataContRelative
 #SBATCH --partition=gpu-small
 #SBATCH --gres=gpu:8
 #SBATCH --nodes=1
 #SBATCH --ntasks=4
 #SBATCH --time=72:00:00
-#SBATCH --output=runall.log
-#SBATCH --error=runaller.err
+#SBATCH --output=runall1.log
+#SBATCH --error=runaller1.err
 #SBATCH --cpus-per-task=8
 
 # 创建日志目录
@@ -23,7 +23,7 @@ srun_parallel () {
     local cuda_id=$4
     local refer_cuda_id=$5
 
-    echo "Running task with model_size=$model_size, truncated=$truncated, cuda_id=$cuda_id, refer_cuda_id=$refer_cuda_id"
+    echo "Running task with model_size=$model_size, truncated=$truncated, cuda_id=$cuda_id, refer_cuda_id=$refer_cuda_id relative=$relative"
 
     python runing_both.py \
         --relative $relative \
@@ -38,10 +38,10 @@ srun_parallel () {
 }
 
 # 启动并行任务
-srun --ntasks=1 --cpus-per-task=8 --gres=gpu:2 bash -c "$(declare -f srun_parallel); srun_parallel absolute 12b truncated 0 1" &
-srun --ntasks=1 --cpus-per-task=8 --gres=gpu:2 bash -c "$(declare -f srun_parallel); srun_parallel absolute 12b untruncated 0 1" &
-srun --ntasks=1 --cpus-per-task=8 --gres=gpu:2 bash -c "$(declare -f srun_parallel); srun_parallel absolute 6.9b truncated 0 1" &
-srun --ntasks=1 --cpus-per-task=8 --gres=gpu:2 bash -c "$(declare -f srun_parallel); srun_parallel absolute 6.9b untruncated 0 1" &
+srun --ntasks=1 --cpus-per-task=8 --gres=gpu:2 bash -c "$(declare -f srun_parallel); srun_parallel relative 12b truncated 0 1" &
+srun --ntasks=1 --cpus-per-task=8 --gres=gpu:2 bash -c "$(declare -f srun_parallel); srun_parallel relative 6.9b truncated 0 1" &
+srun --ntasks=1 --cpus-per-task=8 --gres=gpu:2 bash -c "$(declare -f srun_parallel); srun_parallel relative 2.8b truncated 0 1" &
+srun --ntasks=1 --cpus-per-task=8 --gres=gpu:2 bash -c "$(declare -f srun_parallel); srun_parallel relative 1b truncated 0 1" &
 
 # 等待所有任务结束
 wait
