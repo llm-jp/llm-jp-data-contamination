3ccdbd2e05e9a6450a437110a48a29a685ad35c9
PososAgapo <bchen@kth.se>
2024-09-10 10:48:43 +0900

    fix bugs in generation features

--- sbatch_run.sh
+++ sbatch_run.sh
@@ -42,7 +42,7 @@ srun_parallel () {
 # 启动并行任务
 srun --ntasks=1 --cpus-per-task=8 --gres=gpu:2 bash -c "$(declare -f srun_parallel); srun_parallel absolute 12b untruncated 0 1 0 1" &
 srun --ntasks=1 --cpus-per-task=8 --gres=gpu:2 bash -c "$(declare -f srun_parallel); srun_parallel absolute 6.9b untruncated 0 1 0 2" &
-srun --ntasks=1 --cpus-per-task=8 --gres=gpu:2 bash -c "$(declare -f srun_parallel); srun_parallel relative 2.8b truncated 0 1 0 4" &
+srun --ntasks=1 --cpus-per-task=8 --gres=gpu:2 bash -c "$(declare -f srun_parallel); srun_parallel absolute 12b truncated 0 1 0 1" &
 srun --ntasks=1 --cpus-per-task=8 --gres=gpu:2 bash -c "$(declare -f srun_parallel); srun_parallel relative 1b truncated 0 1 0 4" &
 
 # 等待所有任务结束
