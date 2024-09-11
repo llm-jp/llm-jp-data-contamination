9c81de1b6cdf1adc17747f392a45107b618eca88
PososAgapo <bchen@kth.se>
2024-09-11 11:56:43 +0900

    fix bugs in generation features

--- black_box_mia.py
+++ black_box_mia.py
@@ -130,7 +130,8 @@ def compute_black_box_mia(args):
                     input_length = int(min(tokenized_inputs["attention_mask"].sum(dim=1))/2) if (tokenized_inputs["attention_mask"][0].sum() < args.max_input_tokens) else args.max_input_tokens
                     for _ in tqdm(range(args.generation_samples)):
                         if _ == 0:
-                            zero_temp_generation = model.generate(tokenized_inputs["input_ids"],
+                            zero_temp_generation = model.generate(input_ids=tokenized_inputs["input_ids"][:, :input_length],
+                                                            attention_mask=tokenized_inputs["attention_mask"][:, :input_length],
                                                          temperature=0,
                                                          max_new_tokens=args.max_new_tokens,
                                                         )
@@ -139,7 +140,8 @@ def compute_black_box_mia(args):
                             for i in range(zero_temp_generation["sequences"].shape[0]):
                                 full_decoded[i].append(decoded_sentences[i])
                         else:
-                            generations = model.generate(tokenized_inputs["input_ids"][:, :input_length],
+                            generations = model.generate(input_ids=tokenized_inputs["input_ids"][:, :input_length],
+                                                         attention_mask=tokenized_inputs["attention_mask"][:, :input_length],
                                                      do_sample=True,
                                                      temperature=args.temperature,
                                                      max_new_tokens=args.max_new_tokens,
