da21e0bd12ccce0bb876a4864bc1da735a41db0a
PososAgapo <bchen@kth.se>
2024-09-18 21:35:38 +0900

    fix bugs in generation features

--- sbatch_pac.sh
+++ sbatch_pac.sh
@@ -40,7 +40,7 @@ srun_parallel () {
 }
 
 # 启动并行任务
-srun --ntasks=1 --cpus-per-task=8 --gres=gpu:8 bash -c "$(declare -f srun_parallel); srun_parallel relative 1b truncated 0 0 20" &
+srun --ntasks=1 --cpus-per-task=8 --gres=gpu:8 bash -c "$(declare -f srun_parallel); srun_parallel relative 1b truncated 0 0 32" &
 #srun --ntasks=1 --cpus-per-task=8 --gres=gpu:2 bash -c "$(declare -f srun_parallel); srun_parallel relative 410m truncated 0 1 0 2" &
 #srun --ntasks=1 --cpus-per-task=8 --gres=gpu:2 bash -c "$(declare -f srun_parallel); srun_parallel relative 2.8b  truncated 0 1 0 2" &
 #srun --ntasks=1 --cpus-per-task=8 --gres=gpu:2 bash -c "$(declare -f srun_parallel); srun_parallel relative 1b truncated 0 1 0 2" &
