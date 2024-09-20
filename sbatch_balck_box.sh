c9b7c5fe41596fc1241a5de4c43ef184083e16d2
PososAgapo <bchen@kth.se>
2024-09-20 19:51:24 +0900

    fix bugs in generation features

--- sbatch_balck_box.sh
+++ sbatch_balck_box.sh
@@ -7,7 +7,6 @@
 #SBATCH --time=72:00:00
 #SBATCH --output=RelaBlack12b.log
 #SBATCH --error=RelaBlack12b.err
-#SBATCH --cpus-per-task=8
 #SBATCH --mem=150G
 
 
@@ -66,6 +65,6 @@ srun_parallel () {
 #srun --ntasks=1 --cpus-per-task=8 --gres=gpu:1 bash -c "$(declare -f srun_parallel); srun_parallel relative 12b truncated 0 1 8" &
 #srun --ntasks=1 --cpus-per-task=8 --gres=gpu:1 bash -c "$(declare -f srun_parallel); srun_parallel relative 160m truncated 0 1 8" &
 #srun --ntasks=1 --cpus-per-task=8 --gres=gpu:1 bash -c "$(declare -f srun_parallel); srun_parallel relative 12b truncated 0 2 8" &
-srun --ntasks=1 --cpus-per-task=8 --gres=gpu:1 bash -c "$(declare -f srun_parallel); srun_parallel relative 12b truncated 0 2 4" &
+srun --ntasks=1 --gres=gpu:1 bash -c "$(declare -f srun_parallel); srun_parallel relative 12b truncated 0 2 2" &
 # 等待所有任务结束
 wait
