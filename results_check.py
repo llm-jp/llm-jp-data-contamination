07d442da5d270979111577d3316d3649d6be5ada
PososAgapo <bchen@kth.se>
2024-09-27 15:37:57 +0900

    add configs

---
+++ results_check.py
@@ -0,0 +1,28 @@
+
+save_dir = "mia_dataset_results"
+dataset_idx_list = [0 ,1, 2]
+dataset_name = "local_all"
+relative = "absolute"
+truncated = "truncated"
+model_size = "12b"
+if dataset_name == "local_all" and relative == "absolute":
+    if truncated == "truncated":
+        dataset_list = ['Wikipedia (en)', "StackExchange", 'PubMed Central', "Pile-CC", "HackerNews",
+                    "Github", "FreeLaw", "EuroParl", 'DM Mathematics', "ArXiv", ]
+    elif truncated == "untruncated":
+    # dataset_list = ['Wikipedia (en)', "USPTO Backgrounds", "StackExchange", "Pile-CC", "Github", "FreeLaw"]
+        dataset_list = ['Wikipedia (en)', "StackExchange", "Pile-CC", "Github", "FreeLaw"]
+    elif dataset_name == "local_all" and relative == "relative":
+        dataset_list = ["Wikipedia (en)", "StackExchange", 'PubMed Central', "Pile-CC", "NIH ExPorter", "HackerNews",
+                    "Github", "FreeLaw", "Enron Emails", "DM Mathematics", "ArXiv"]
+min_len_list = range(0, 1000, 100) if relative == "absolute" else range(0, 100, 10)
+for dataset_name in dataset_list:
+    for dataset_idx in [0, 1, 2]:
+        for min_len in min_len_list:
+            for detection_method in ["loss", "ccd", "pca"]:
+                if os.path.exists(
+                        f"{save_dir}_{dataset_idx}/{dataset_name}/{relative}/{truncated}/{min_len}_{model_size}_{detection_method}_dict.pkl"):
+                    print("ok")
+                else:
+                    print(f"{model_size} {dataset_name} {min_len} {detection_method} not finished")
+
