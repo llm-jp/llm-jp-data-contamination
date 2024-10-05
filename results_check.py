60a33789d6eaa6c20400bbdcd5976dfb4025a85a
PososAgapo <bchen@kth.se>
2024-10-05 17:10:20 +0900

    add configs

--- results_check.py
+++ results_check.py
@@ -2,7 +2,7 @@ import os
 save_dir = "mia_dataset_results"
 dataset_idx_list = [0 ,1, 2]
 dataset_name = "local_all"
-relative = "absolute"
+relative = "relative"
 truncated = "truncated"
 model_size_list = ["160m", "410m", "1b", "2.8b", "6.9b", "12b"]
 #["160m", "410m", "1b", "2.8b", "6.9b", "12b"]
