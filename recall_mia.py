91c66716c9bdceeec1b7d24fa5df1d7d709fdc8a
PososAgapo <bchen@kth.se>
2024-10-27 18:22:30 +0900

    add configs

--- recall_mia.py
+++ recall_mia.py
@@ -31,9 +31,9 @@ def compute_recall(args):
         for min_len in length_list:
             args.min_len = min_len
             for dataset_name in dataset_names:
-                if os.path.exists(f"{args.save_dir}_{args.dataset_idx}/{dataset_name}/{args.relative}/{args.truncated}/{args.min_len}_{args.model_size}_recall_dict.pkl"):
-                    print(f"{dataset_idx} {dataset_name} {args.min_len} {args.model_size} finished")
-                    continue
+                #if os.path.exists(f"{args.save_dir}_{args.dataset_idx}/{dataset_name}/{args.relative}/{args.truncated}/{args.min_len}_{args.model_size}_recall_dict.pkl"):
+                #    print(f"{dataset_idx} {dataset_name} {args.min_len} {args.model_size} finished")
+                #    continue
                 df = pd.DataFrame()
                 dataset = obtain_dataset(dataset_name, args)
                 recall_dict = {}
